ea1d56b8b5721021d3935bc29921d9e5a91d6cb8
Michael Cheng <mcheng0@chicagobooth.edu>
2016-11-07 14:39:45 -0600

    final

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,34 @@
 class CalculationsController < ApplicationController
   def instructions
+
     render("instructions.html.erb")
+
+  end
+  def square
+    @number = params[:number].to_f
+    @answer = @number**2
+
+    render("square.html.erb")
+  end
+  def square_root
+    @number = params[:number].to_f
+    @answer = @number**0.5
+
+    render("square_root.html.erb")
+  end
+  def random
+    @number1 = params[:min].to_i
+    @number2 = params[:max].to_i
+    @random = rand(@number1..@number2).to_i
+
+    render("random.html.erb")
+  end
+  def payment
+    @rate = params[:interest_rate].to_f/100
+    @years = params[:number_of_years].to_f
+    @principal = params[:principal_value].to_f
+    @payment = (@rate/1200+((@rate/1200)/((1+(@rate/1200))**(@years*12)-1)))*@principal
+
+    render("payment.html.erb")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square_root/:number", { :controller => "calculations", :action => "square_root"})
+  get("/square/:number", { :controller => "calculations", :action => "square"})
+  get("/random/:min/:max", { :controller => "calculations", :action => "random"})
+  get("/payment/:interest_rate/:number_of_years/:principal_value", { :controller => "calculations", :action => "payment"})
 end
